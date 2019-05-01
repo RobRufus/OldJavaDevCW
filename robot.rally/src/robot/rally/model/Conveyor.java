63f14b7f26845d7fc9e3fe427ea76e2ab92f48e6
Claxtont <claxtont@aston.ac.uk>
2019-05-01 18:23:12 +0100

    very minor conveyor changes

--- robot.rally/src/robot/rally/model/Conveyor.java
+++ robot.rally/src/robot/rally/model/Conveyor.java
@@ -11,8 +11,8 @@ public class Conveyor extends GridEntity  {
 		super(locationX, locationY); 
 	}
 	
-	
-	public void activate()
+	@Override
+	public void activate(Board currentBoard,int locationX, int locationY, Board updatedBoard)
 	{
 		//override activate function in GridEntity
 		
