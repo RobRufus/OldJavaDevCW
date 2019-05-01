4baa447bc97ae304325749d2f72058db70f06008
Claxtont <claxtont@aston.ac.uk>
2019-05-01 19:55:46 +0100

    gui start, fuck me this is hard and confusing

--- robot.rally/src/robot/rally/game/Launcher.java
+++ robot.rally/src/robot/rally/game/Launcher.java
@@ -1,6 +1,13 @@
 package robot.rally.game;
 
-public class Launcher {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import robot.rally.view.LauncherController;
+
+public class Launcher extends Application {
 	
 	int numOfPlayers;
 	int gameType;
@@ -10,12 +17,25 @@ public class Launcher {
 	
 	public static void main(String[] args){
 		System.out.println("Hello World");
-		
-		Launcher launcher = new Launcher();
-		launcher.buildSim();
+		launch(args);
 		
 	}
-
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		
+		final FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("LauncherView.fxml"));
+		loader.setController(new LauncherController());;
+		final Parent root = loader.load();
+		final Scene scene = new Scene(root, 400, 300);
+		primaryStage.setTitle("RoboRally launcher");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	
 	public Launcher() 
 	{
 		//constructor for launcher class
---
+++ robot.rally/src/robot/rally/view/LauncherController.java
@@ -0,0 +1,18 @@
+package robot.rally.view;
+
+import javafx.fxml.FXML;
+
+
+public class LauncherController {
+	
+	public LauncherController() {
+		
+		
+	}
+	
+	@FXML public void initialize() {}
+	@FXML public void chooseFilePressed() {}
+	@FXML public void donePressed() {}
+		
+
+}
