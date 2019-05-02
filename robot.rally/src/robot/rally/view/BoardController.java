29f934591e16d64fc4d9ebf08a0ad1e39f587f75
Claxtont <claxtont@aston.ac.uk>
2019-05-02 15:57:05 +0100

    tom is being stupid

---
+++ robot.rally/src/robot/rally/view/BoardController.java
@@ -0,0 +1,21 @@
+package robot.rally.view;
+
+import javafx.fxml.FXML;
+import robot.rally.game.Launcher;
+
+public class BoardController {
+	
+	
+	
+@FXML public void ForwardsPressed() {
+		
+		System.out.println("buttz");
+	}
+	public BoardController() {}
+	
+	@FXML
+    private void initialize() {}
+	
+	
+    
+}
