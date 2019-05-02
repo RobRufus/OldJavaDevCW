e5e8ae264c1c5acd5a31c7bbd84dbcf884805e09
Claxtont <claxtont@aston.ac.uk>
2019-05-02 14:01:04 +0100

    AHHHHHHHHHHHHHHHHHHHHHHH I LOST SO MUCH WORK

--- robot.rally/src/robot/rally/game/Launcher.java
+++ robot.rally/src/robot/rally/game/Launcher.java
@@ -1,12 +1,18 @@
 package robot.rally.game;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import robot.rally.IO.Input;
 import robot.rally.view.LauncherController;
+import javafx.scene.layout.AnchorPane;
+
+
+
 
 public class Launcher extends Application {
 	
@@ -17,45 +23,97 @@ public class Launcher extends Application {
 	String boardFilePath;
 	Input input;
 	Simulator simulator;
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("RoboRally launcher");
+
+        initRootLayout();
+        
+        showLauncher();
+    }
+    
+    /**
+     * Initializes the root layout.
+     */
+public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Launcher.class.getResource("RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+public void showLauncher() {
+    try {
+        // Load launcher screen.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Launcher.class.getResource("LauncherView.fxml"));
+        AnchorPane launcher = (AnchorPane) loader.load();
+        
+        // Set launcher into the centre of root layout.
+        rootLayout.setCenter(launcher);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+public Stage getPrimaryStage() {
+	return primaryStage;
 	
-	public static void main(String[] args){
+}
+
+
+public static void main(String[] args){
 		System.out.println("Hello World");
 		launch(args);	
 	}
 	
-	@Override
-	public void start(Stage primaryStage) throws Exception{
-		
-		final FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("LauncherView.fxml"));
-		loader.setController(new LauncherController());;
-		final Parent root = loader.load();
-		final Scene scene = new Scene(root, 400, 300);
-		primaryStage.setTitle("RoboRally launcher");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
+//	@Override
+//	public void start(Stage primaryStage) throws Exception{
+//		
+//		final FXMLLoader loader = new FXMLLoader();
+//		loader.setLocation(getClass().getResource("LauncherView.fxml"));
+//		loader.setController(new LauncherController());;
+//		final Parent root = loader.load();
+//		final Scene scene = new Scene(root, 400, 300);
+//		this.primaryStage.setTitle("RoboRally launcher");
+//		this.primaryStage.setScene(scene);
+//		this.primaryStage.show();
+//		
+//	}
 	
 	
-	public Launcher(String brdFilePath, String actFilePath) 
-	{
-		//constructor for launcher class
-		boardFilePath = brdFilePath;
-		actionFilePath = actFilePath;
-		Input input = new Input();
-		Simulator simulator = new Simulator();
-		simulator.generateBoard(input.readFile(boardFilePath));
-		input.readFile(actionFilePath);		
-	}
+//	public Launcher(String brdFilePath, String actFilePath) 
+//	{
+//		//constructor for launcher class
+//		boardFilePath = brdFilePath;
+//		actionFilePath = actFilePath;
+//		Input input = new Input();
+//		Simulator simulator = new Simulator();
+//		simulator.generateBoard(input.readFile(boardFilePath));
+//		input.readFile(actionFilePath);		
+//	}
 	
 	
-	public void buildSim()
-	{
-		/* function which allows user to change settings and uses them
-		to build a simulator class*/
-		
-	}
+//	public void buildSim()
+//	{
+//		/* function which allows user to change settings and uses them
+//		to build a simulator class*/
+//		
+//	}
 	
 	
 
